b52ec314c5e9a82fa9e86ec52b99e3db7a225bf7
itzhakshtern <itzhak7915@gmail.com>
2023-03-12 20:42:49 +0200

    add: encription to password user - using with bcript library

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,9 +18,11 @@ export class AuthService {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
+    const encriptionPassword = await bcrypt.hash(password, salt);
     const user = this.usersRepository.create({
       username,
-      password,
+      password: encriptionPassword,
     });
     try {
       await this.usersRepository.save(user);
